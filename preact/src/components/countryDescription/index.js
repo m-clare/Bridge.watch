02320905fd0a7b547e2d21280ebb98f4a5832241
Maryanne Wachter <maryanne.wachter@gmail.com>
2021-10-15 19:40:12 -0700

    added colorblind palette, changed summary to card

--- preact/src/components/countryDescription/index.js
+++ preact/src/components/countryDescription/index.js
@@ -1,69 +1,114 @@
 import { h } from "preact";
+import { useState } from "preact/hooks";
 import htm from "htm";
 import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
 import Link from "@mui/material/Link";
 import Typography from "@mui/material/Typography";
+
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+
+import Button from "@mui/material/button";
+import { styled } from "@mui/material/styles";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import Collapse from "@mui/material/Collapse";
+import IconButton from "@mui/material/IconButton";
 const html = htm.bind(h);
 
+const ExpandMore = styled((props) => {
+  const { expand, ...other } = props;
+  return html`<${IconButton} ...${other} />`;
+})(({ theme, expand }) => ({
+  transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
+  marginLeft: "auto",
+  transition: theme.transitions.create("transform", {
+    duration: theme.transitions.duration.shortest,
+  }),
+}));
+
 export function CountryDescription({ summaryType, keyValues }) {
+  const [expanded, setExpanded] = useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
   if (summaryType === "rating") {
     const field = keyValues.field;
     const count = keyValues.count;
     return html`
     <${Grid} item container>
-      <${Paper} variant=${"outlined"} style=${"padding: 15px"}>
+      <${Card} variant=${"outlined"}>
         <${Grid} item xs=${12}>
-          <${Typography} variant="h5" component="h2">Plot Summary</${Typography}>
-          <p>This map aggregates the locations of ${Number(
-            Math.round(count / 100) * 100
-            ).toLocaleString()} bridges in the U.S. with their overall "rating" as encoded in the <${Link} underline=${"hover"} href="https://www.fhwa.dot.gov/bridge/nbi.cfm"><b> 2020 National Bridge Inventory</b></${Link}> on a scale of 0 to 9. Bridges that are missing ratings are omitted from the plot. The hexagon size represents the number of bridges in the vicinity, while the color represents the most common rating in the corresponding histogram. Additional filtering based on material and year built ranges can be performed using the dropdown menus above. </p>
+          <${CardContent}>
+            <${Typography} variant="h5" component="h2">Plot Summary</${Typography}>
+            <p>This map aggregates the locations of ${Number(
+              Math.round(count / 100) * 100
+            ).toLocaleString()} bridges in the U.S. with their overall "rating" as encoded in the <${Link} underline=${"hover"} href="https://www.fhwa.dot.gov/bridge/nbi.cfm"><b> 2020 National Bridge Inventory</b></${Link}> on a scale of 0 to 9. Bridges that are missing ratings are omitted from the plot. The hexagon size represents the number of bridges in the vicinity, while the color represents the most common rating in the corresponding histogram. Additional filtering can be performed using the options above. </p>
+          </${CardContent}>
+          <${CardActions} disableSpacing>
+            <${Button} variant="text" onClick=${handleExpandClick}>More information</${Button}>
+            <${ExpandMore}
+              expand=${expanded}
+              onClick=${handleExpandClick}
+              aria-expanded=${expanded}
+              aria-label="more information"
+              >
+              <${ExpandMoreIcon} />
+            </${ExpandMore}>
+          </${CardActions}>
+          <${Collapse} in=${expanded} timeout="auto" unmountOnExit>
+            <${CardContent}>
+              <${Grid} container spacing=${2}>
+                <${Grid} item xs=${12}>
+                  <${Typography} variant="h5" component="h3">Rating Guide:</${Typography}>
+                </${Grid}>
+                <${Grid} item xs=${12} sm=${4}>
+                  <${Typography} variant="h6" component="h4">"Poor" Conditions</${Typography}>
+                  <ul>
+                    <li>0 - Failed Condition (Not In Service)</li>
+                    <li>1 - Imminent Failure Condition (Not In Service)</li>
+                    <li>2 - Critical Condition</li>
+                    <li>3 - Serious Condition</li>
+                    <li>4 - Poor Condition</li>
+                  </ul>
+                </${Grid}>
+                <${Grid} item xs=${12} sm=${4}>
+                  <${Typography} variant="h6" component="h4">"Fair" Conditions</${Typography}>
+                  <ul>
+                    <li>5 - Fair Condition</li>
+                    <li>6 - Satisfactory Condition</li>
+                  </ul>
+                </${Grid}>
+                <${Grid} item xs=${12} sm=${4}>
+                  <${Typography} variant="h6" component="h4">"Good" Conditions</${Typography}>
+                  <ul>
+                    <li>7 - Good Condition</li>
+                    <li>8 - Very Good Condition</li>
+                    <li>9 - Excellent Condition</li>
+                  </ul>
+                </${Grid}>
+                <${Grid} item xs=${12}>
+                  <p style=${"margin: 0px"}>The lowest rating for a specific bridge component (superstructure, substructure, or deck) determines the overall rating of Good/Fair/Poor.</p>
+                  <p>For example, a bridge with:</p>
+                  <ul>
+                    <li>7 - "Good" - substructure</li>
+                    <li>8 - "Very Good" - superstructure</li>
+                    <li>8 - "Very Good" - deck</li>
+                  </ul>
+                  <p>would be assigned a lowest rating of 7 (the value indicated in the plot) and overall assessment of "Good".</p>
+                </${Grid}>
+              </${Grid}>
+            </${CardContent}>
+          </${Collapse}>
         </${Grid}>
-        <${Grid} container spacing=${2}>
-          <${Grid} item xs=${12}>
-            <${Typography} variant="h5" component="h3">Rating Guide:</${Typography}>
-          </${Grid}>
-          <${Grid} item xs=${12} sm=${4}>
-            <${Typography} variant="h6" component="h4">"Poor" Conditions</${Typography}>
-            <ul>
-              <li>0 - Failed Condition (Not In Service)</li>
-              <li>1 - Imminent Failure Condition (Not In Service)</li>
-              <li>2 - Critical Condition</li>
-              <li>3 - Serious Condition</li>
-              <li>4 - Poor Condition</li>
-            </ul>
-          </${Grid}>
-          <${Grid} item xs=${12} sm=${4}>
-            <${Typography} variant="h6" component="h4">"Fair" Conditions</${Typography}>
-            <ul>
-              <li>5 - Fair Condition</li>
-              <li>6 - Satisfactory Condition</li>
-            </ul>
-          </${Grid}>
-          <${Grid} item xs=${12} sm=${4}>
-            <${Typography} variant="h6" component="h4">"Good" Conditions</${Typography}>
-            <ul>
-              <li>7 - Good Condition</li>
-              <li>8 - Very Good Condition</li>
-              <li>9 - Excellent Condition</li>
-            </ul>
-          </${Grid}>
-          <${Grid} item xs=${12}>
-            <p style=${"margin: 0px"}>The lowest rating for a specific bridge component (superstructure, substructure, or deck) determines the overall rating of Good/Fair/Poor.</p>
-            <p>For example, a bridge with:</p>
-            <ul>
-              <li>7 - "Good" - substructure</li>
-              <li>8 - "Very Good" - superstructure</li>
-              <li>8 - "Very Good" - deck</li>
-            </ul>
-            <p>would be assigned a lowest rating of 7 (the value indicated in the plot) and overall assessment of "Good".</p>
-          </${Grid}>
-        </${Grid}>
-      </${Paper}>
+      </${Card}>
     </${Grid}>`;
-    } else if (summaryType === "year built") {
+  } else if (summaryType === "year built") {
     return;
-    } else {
+  } else {
     return;
   }
 }
--- preact/src/components/hexbinMap/index.js
+++ preact/src/components/hexbinMap/index.js
@@ -5,7 +5,7 @@ import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
 import Link from "@mui/material/Link";
-import Typography from "@mui/material/Typography"
+import Typography from "@mui/material/Typography";
 import * as d3 from "d3";
 import { hexbin } from "d3-hexbin";
 import { mesh } from "topojson-client";
@@ -28,7 +28,7 @@ const stdMargin = 30;
 const projection = d3
   .geoAlbersUsa()
   .scale(scaleValue)
-  .translate([width * 0.5, height * 0.5])
+  .translate([width * 0.5, height * 0.5]);
 const myHexbin = hexbin()
   .extent([
     [0, 0],
@@ -37,34 +37,46 @@ const myHexbin = hexbin()
   .radius(10);
 
 const ratings = {
-  0: 'Failed',
-  9: 'Excellent'
-}
+  0: "Failed",
+  9: "Excellent",
+};
 
 // Color settings (initial hardcode for ratings data)
 // potential color scales - numerical range, categorical, categorical/numerical
 
 const ratingColor = d3
   .scaleLinear()
-      .domain([0, 3, 6, 9])
-      .range(["darkred", "red", "yellow", "darkgreen"])
-  .interpolate(d3.interpolateRgb.gamma(2.2));
-
-const barChartColor = d3
-  .scaleLinear()
   .domain([0, 3, 6, 9])
   .range(["darkred", "red", "yellow", "darkgreen"])
   .interpolate(d3.interpolateRgb.gamma(2.2));
 
+const ratingColorblind = d3
+  .scaleLinear()
+  .domain(d3.range(0, 10))
+  .range([
+    "#a50026",
+    "#d73027",
+    "#f46d43",
+    "#fdae61",
+    "#fee090",
+    "#e0f3f8",
+    "#abd9e9",
+    "#74add1",
+    "#4575b4",
+    "#313695",
+  ]).interpolate(d3.interpolateRgb.gamma(2.2));
+
+
 const colorDict = {
-  "rating": ratingColor
-}
+  rating: ratingColor,
+  ratingCB: ratingColorblind
+};
 
 export function HexbinChart({ bridgeData }) {
   const [activeHex, setActiveHex] = useState({});
   const [totalValues, setTotalValues] = useState({});
   const [hexSelected, setHexSelected] = useState(false);
-  const [colorPalette, setColorPalette] = useState("rating")
+  const [colorPalette, setColorPalette] = useState("rating");
 
   const d3Container = useRef(null);
 
@@ -92,9 +104,9 @@ export function HexbinChart({ bridgeData }) {
 
   useEffect(() => {
     if (!isEmpty(bridgeData)) {
-      setColorPalette(bridgeData.field)
+      setColorPalette(bridgeData.field);
     }
-  }, [bridgeData])
+  }, [bridgeData]);
 
   useEffect(() => {
     if (!isEmpty(bridgeData) && d3Container.current) {
@@ -110,33 +122,33 @@ export function HexbinChart({ bridgeData }) {
         [0, myHexbin.radius() * Math.SQRT2]
       );
 
-      const color = colorDict[bridgeData.field]
+      const color = colorDict[colorPalette];
 
       //TODO:  add legend only once... probe this further
       if (!document.getElementById("legend")) {
-      svg
-        .append("g")
-        .attr('id', 'legend')
-        .attr("transform", `translate(${0.6 * width}, ${stdMargin})`)
-        .append(() =>
-          legend({
-            color: color,
-            width: width * 0.3,
-            tickFormat: ".0f",
-            tickSize: 0,
-            ticks: 8,
-            tickExtremes: [ratings[0], ratings[9]]
-          })
-        );
+        svg
+          .append("g")
+          .attr("id", "legend")
+          .attr("transform", `translate(${0.6 * width}, ${stdMargin})`)
+          .append(() =>
+            legend({
+              color: color,
+              width: width * 0.3,
+              tickFormat: ".0f",
+              tickSize: 0,
+              ticks: 8,
+              tickExtremes: [ratings[0], ratings[9]],
+            })
+          );
       }
 
       const getHex = () => {
         if (!document.getElementById("hexes")) {
-          svg.append("g").attr("id", "hexes")
+          svg.append("g").attr("id", "hexes");
         }
-        return svg.select('#hexes');
-        }
-      const hexNode = getHex()
+        return svg.select("#hexes");
+      };
+      const hexNode = getHex();
 
       // add hexes
       hexNode
