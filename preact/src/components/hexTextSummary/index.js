eaee572a75a93246014384c6a83477930372aa12
Maryanne Wachter <maryanne.wachter@gmail.com>
2021-10-18 16:11:31 -0700

    simplified text summary info

--- preact/src/components/hexTextSummary/index.js
+++ preact/src/components/hexTextSummary/index.js
@@ -8,36 +8,29 @@ import ListItem from "@mui/material/ListItem";
 const html = htm.bind(h);
 
 export function HexTextSummary({ selected, objData, natData, field }) {
-  // state needs to be used because objData may be empty to start
+  let data;
+
   if (selected) {
-    return html`
+    data = objData
+  } else {
+    data = natData
+  }
+  return html`
       <div style=${"min-height: 220px"}>
-        <${Typography} variant="h5" component="h2">Selected Hex Properties</${Typography}> 
+        <${Typography} variant="h5" component="h2">
+          ${selected ? 'Selected Hex Properties' : 'National Bridge Properties'}
+        </${Typography}> 
         <${List} dense=${true}>
-        <${ListItem}>Number of Bridges: ${objData.count}</${ListItem}>
-        <${ListItem}>${field} minimum: ${objData.min}</${ListItem}>
-        <${ListItem}>${field} maximum: ${objData.max}</${ListItem}>
-        <${ListItem}>${field} average: ${objData.avg}</${ListItem}>
-        <${ListItem}>${field} median: ${objData.median}</${ListItem}>
-        <${ListItem}>${field} mode: ${objData.mode}</${ListItem}>
-        <${ListItem}>
-          Center Coordinate: ${objData.hexLocation[1]}°N, ${-objData.hexLocation[0]}°W
-        </${ListItem}>
+        <${ListItem}>Number of Bridges: ${data.count}</${ListItem}>
+        <${ListItem}>${field} minimum: ${data.min}</${ListItem}>
+        <${ListItem}>${field} maximum: ${data.max}</${ListItem}>
+        <${ListItem}>${field} average: ${data.avg}</${ListItem}>
+        <${ListItem}>${field} median: ${data.median}</${ListItem}>
+        <${ListItem}>${field} mode: ${data.mode}</${ListItem}>
+         ${selected ? html`<${ListItem}>
+          Center Coordinate: ${data.hexLocation[1]}°N, ${-data.hexLocation[0]}°W
+  </${ListItem}>` : null}
         </${List}>
     </div>
     `;
-  } else
-    return html`
-      <div style=${"min-height: 220px"}>
-        <${Typography} variant="h5" component="h2">National Bridge Properties</${Typography}>
-        <${List} dense=${true}>
-        <${ListItem}>Number of Bridges: ${natData.count}</${ListItem}>
-        <${ListItem}>${field} minimum: ${natData.min}</${ListItem}>
-        <${ListItem}>${field} maximum: ${natData.max}</${ListItem}>
-        <${ListItem}>${field} average: ${natData.avg}</${ListItem}>
-        <${ListItem}>${field} median: ${natData.median}</${ListItem}>
-        <${ListItem}>${field} mode: ${natData.mode}</${ListItem}>
-        <${ListItem}>  </${ListItem}>
-        </${List}>
-      </div>`;
-}
+  } 
