dc09518d33f1ede09939fc3b0677c8ede210ea92
Maryanne Wachter <maryanne.wachter@gmail.com>
2021-11-09 20:44:46 -0800

    fixed django views for partition type aggregation

--- app/national/views.py
+++ app/national/views.py
@@ -3,10 +3,11 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import *
 from .serializers import *
-from django.db.models import F
+from django.db.models import F, Count
 from .writers import get_streaming_response
 from django_pandas.io import read_frame
 from django.http import HttpResponse
+from django.http import JsonResponse
 # Create your views here.
 
 @api_view(["GET"])
@@ -157,5 +158,114 @@ def national_bridges_csv(request):
         # return response
 
         return get_streaming_response(bridges, fields)
+    else:
+        return Response("", status=status.HTTP_400_BAD_REQUEST)
+
+def check_conditions(query_dict, value, bool_tuple):
+    deck_bool = query_dict["deck_cond"] == value
+    superstructure_bool = query_dict["superstructure_cond"] == value
+    substructure_bool = query_dict["substructure_cond"] == value
+    # check with tuple
+    return (deck_bool, superstructure_bool, substructure_bool) == bool_tuple
+
+@api_view(["GET"])
+def bridge_conditions(request):
+    fields = []
+    condition_dictionary = {"G": [7, 8, 9], "F": [5, 6], "P": [0, 1, 2, 3, 4]}
+
+    condition_ratings = {
+        "9": "Excellent",
+        "8": "Very Good",
+        "7": "Good",
+        "6": "Satisfactory",
+        "5": "Fair",
+        "4": "Poor",
+        "3": "Serious",
+        "2": "Critical",
+        "1": "Imminent Failure",
+        "0": "Failed",
+    }
+    cond_dict = {"G": {}, "F": {}, "P": {}}
+    output_dict = {"name": "2021 Bridge Condition Data", "children": []}
+    # TODO: Make more generic function to generate permutations
+    permutation_dict = {
+        "all components": (True, True, True),
+        "deck and substructure": (True, False, True),
+        "deck and superstructure": (True, True, False),
+        "super/substructure": (False, True, True),
+        "deck": (True, False, False),
+        "superstructure": (False, True, False),
+        "substructure": (False, False, True),
+    }
+    if request.method == "GET":
+        state = request.query_params.get("state")
+        bridges = Bridge.objects.all()
+        bridges = bridges.exclude(lowest_rating__code=None)
+        fields.append("rating")
+
+        if ('rating' in fields):
+            bridges = bridges.annotate(rating=F('lowest_rating__code'))
+        if state is not None:
+            state_list = state.split(',')
+            bridges = bridges.filter(state__fips_code__in=state_list)
+            bridges = bridges.annotate(state_name=F('state__name'))
+
+        bridges = bridges.annotate(material=F('structure_kind__code'))
+        bridges = bridges.annotate(bridge_cond=F('bridge_condition'))
+        bridges = bridges.annotate(deck_cond=F('deck_condition__code'))
+        bridges = bridges.annotate(superstructure_cond=F('superstructure_condition__code'))
+        bridges = bridges.annotate(substructure_cond=F('substructure_condition__code'))
+        fields.extend(["bridge_condition",
+                       "deck_cond",
+                       "superstructure_cond",
+                       "substructure_cond"])
+
+        # limit query results for troubleshooting
+        limit = request.query_params.get("limit")
+        if (limit != None):
+            num_limit = int(limit)
+            bridges = bridges[:num_limit]
+
+        bridge_cond_by_field = list(bridges.values(*fields).annotate(count=Count("pk")))
+
+        for key, value_list in condition_dictionary.items():
+            # first filter list of dictionaries to only those with relevant lowest rating
+            for value in value_list:
+                cond_dict[key][value] = [
+                    x
+                    for x in bridge_cond_by_field
+                    if (x["bridge_condition"] == key and x["rating"] == value)
+                ]
+        for condition_letter, rating_dictionary in cond_dict.items():
+            name_dict = {"name": condition_letter, "children": []}
+            for rating, matching_counts in rating_dictionary.items():
+                values_dict = {
+                    "name": f"{condition_ratings[str(rating)]} Condition ({str(rating)})",
+                    "children": [],
+                }
+                child_list = []
+                # Initialize condition dictionaries
+                component_counts = {}
+                for component_state, bool_tuple in permutation_dict.items():
+                    component_counts[component_state] = 0
+                for count in matching_counts:
+                    for component_state, bool_tuple in permutation_dict.items():
+                        condition_match = check_conditions(
+                            count, rating, bool_tuple
+                        )
+                        if condition_match:
+                            component_counts[component_state] += count["count"]
+                # create arrays for d3
+                for component, count in component_counts.items():
+                    child_list.append(
+                        {
+                            "name": component + " at " + str(rating),
+                            "value": count,
+                        }
+                    )
+                values_dict["children"] = child_list
+                name_dict["children"].append(values_dict)
+            output_dict["children"].append(name_dict)
+        return JsonResponse(output_dict)
     else:
         return Response("", status=status.HTTP_400_BAD_REQUEST)
